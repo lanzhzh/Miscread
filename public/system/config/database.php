ab77de1a457baeb236ef554783dba380bb3cd607
lanzhzh <lansysu@gmail.com>
2015-07-21 23:56:32 -0400

    v1.0

---
+++ public/system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'lanzhzh',
+  'password' => '12345ssdlh',
+  'dbname' => 'valseonline',
+);
+$config['slave'] = false;
