ba0770f9f2c5b83b1f6608d6dc352345cb028f45
lanzhzh <lansysu@gmail.com>
2015-07-22 00:31:13 -0400

    recover to original files to solve the problem that article cannot be given

--- public/system/config/notification.php
+++ public/system/config/notification.php
@@ -3,29 +3,29 @@
 $config['action_details'][notify_class::TYPE_PEOPLE_FOCUS] = array(
 	'user_setting' => 1,
 	'combine' => 0,
-	'desc' => AWS_APP::lang()->_t('当有人关注我')
+	'desc' => AWS_APP::lang()->_t('有人关注了我')
 );
 
 $config['action_details'][notify_class::TYPE_NEW_ANSWER] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => AWS_APP::lang()->_t('我关注的问题有了新回复')
+	'desc' => AWS_APP::lang()->_t('我关注的问题有了新的回复')
 );
 
 $config['action_details'][notify_class::TYPE_INVITE_QUESTION] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => AWS_APP::lang()->_t('有人邀请我回答问题')
+	'desc' => AWS_APP::lang()->_t('有人邀请我回复问题')
 );
 
 $config['action_details'][notify_class::TYPE_QUESTION_COMMENT] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => AWS_APP::lang()->_t('我的问题有了新回复')
+	'desc' => AWS_APP::lang()->_t('我的问题被评论')
 );
 
 $config['action_details'][notify_class::TYPE_ANSWER_COMMENT] = array(
-	'user_setting' => 0,
+	'user_setting' => 1,
 	'combine' => 1,
 	'desc' => AWS_APP::lang()->_t('我的问题评论被回复')
 );
@@ -33,41 +33,41 @@ $config['action_details'][notify_class::TYPE_ANSWER_COMMENT] = array(
 $config['action_details'][notify_class::TYPE_COMMENT_AT_ME] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => AWS_APP::lang()->_t('当有人 @ 我')
+	'desc' => AWS_APP::lang()->_t('有问题评论提到我')
 );
 
 $config['action_details'][notify_class::TYPE_ANSWER_COMMENT_AT_ME] = array(
-	'user_setting' => 0,
+	'user_setting' => 1,
 	'combine' => 1,
 	'desc' => AWS_APP::lang()->_t('有回答评论提到我')
 );
 
 $config['action_details'][notify_class::TYPE_ANSWER_AT_ME] = array(
-	'user_setting' => 0,
+	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => AWS_APP::lang()->_t('当有人 @ 我')
+	'desc' => AWS_APP::lang()->_t('有回答提到我')
 );
 
 $config['action_details'][notify_class::TYPE_ANSWER_AGREE] = array(
-	'user_setting' => 0,
+	'user_setting' => 1,
 	'combine' => 1,
 	'desc' => AWS_APP::lang()->_t('我的回复收到赞同')
 );
 
 $config['action_details'][notify_class::TYPE_ANSWER_THANK] = array(
-	'user_setting' => 0,
+	'user_setting' => 1,
 	'combine' => 1,
 	'desc' => AWS_APP::lang()->_t('我的回复收到感謝')
 );
 
 $config['action_details'][notify_class::TYPE_QUESTION_THANK] = array(
-	'user_setting' => 0,
+	'user_setting' => 1,
 	'combine' => 1,
 	'desc' => AWS_APP::lang()->_t('我发布的问题收到感謝')
 );
 
 $config['action_details'][notify_class::TYPE_MOD_QUESTION] = array(
-	'user_setting' => 0,
+	'user_setting' => 1,
 	'combine' => 1,
 	'desc' => AWS_APP::lang()->_t('我的问题被编辑')
 );
@@ -79,7 +79,7 @@ $config['action_details'][notify_class::TYPE_REMOVE_ANSWER] = array(
 );
 
 $config['action_details'][notify_class::TYPE_REDIRECT_QUESTION] = array(
-	'user_setting' => 0,
+	'user_setting' => 1,
 	'combine' => 1,
 	'desc' => AWS_APP::lang()->_t('我发布的问题被重定向')
 );
@@ -93,11 +93,11 @@ $config['action_details'][notify_class::TYPE_CONTEXT] = array(
 $config['action_details'][notify_class::TYPE_ARTICLE_NEW_COMMENT] = array(
 	'user_setting' => 1,
 	'combine' => 1,
-	'desc' => AWS_APP::lang()->_t('我的文章有了新评论')
+	'desc' => AWS_APP::lang()->_t('我的文章被评论')
 );
 
 $config['action_details'][notify_class::TYPE_ARTICLE_COMMENT_AT_ME] = array(
-	'user_setting' => 0,
+	'user_setting' => 1,
 	'combine' => 1,
 	'desc' => AWS_APP::lang()->_t('有文章评论提到我')
 );
